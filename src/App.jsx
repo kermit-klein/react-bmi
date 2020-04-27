11d0b94c10f6a8cc7eaa832ac9dd838dfc8500eb
allezz <aerbay@gmail.com>
2020-04-27 11:44:46 +0200

    Add Form specs,Add Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return <Form />;
   }
 }
 export default App;
